bfa3342d933afda2df7f53034e722fb31d8886dd
Kimchelly <kmtao88@gmail.com>
2015-06-29 10:21:29 -0400

    Added basic collection view

---
+++ LazyCalendar/CalendarCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarCollectionViewCell.swift
+//  LazyCalendar
+//
+//  Created by Ying Tao on 6/29/15.
+//  Copyright (c) 2015 Kim. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+
+    
+}
---
+++ LazyCalendar/CalendarCollectionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CalendarCollectionViewController.swift
+//  LazyCalendar
+//
+//  Created by Ying Tao on 6/29/15.
+//  Copyright (c) 2015 Kim. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCollectionViewController: UICollectionViewController, UICollectionViewDataSource {
+    private let reuseIdentifier = "DayCell"
+    private let daysInMonth = [1, 2, 3, 4 , 5, 6, 7]
+    private let numCellsInMonth = 7
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return numCellsInMonth
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CalendarCollectionViewCell
+        cell.dayLabel.text = "\(daysInMonth[indexPath.row])"
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
