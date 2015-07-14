06b39c04c39825ce9cedb51da76cd36b1965ba67
Kimchelly <kmtao88@gmail.com>
2015-07-14 11:49:47 -0400

    Alarm view shows, need to fix table view sizing

--- LazyCalendar/ChangeEventViewController.swift
+++ LazyCalendar/ChangeEventViewController.swift
@@ -22,6 +22,8 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
     private let eventDateStartPicker = UIDatePicker()
     private let eventDateEndPicker = UIDatePicker()
     
+    //private let alarmDateSwitch = UISwitch()
+    
     // Text field for event name
     @IBOutlet weak var eventNameTextField: UITextField!
     
@@ -35,22 +37,48 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
     
     // Toggles alarm option on/off
     @IBOutlet weak var alarmSwitch: UISwitch!
+    @IBOutlet weak var alarmDateSwitch: UISwitch!
     
+    @IBOutlet weak var alarmTimeMainLabel: UILabel!
+    @IBOutlet weak var alarmTimeDetailsLabel: UILabel!
     // Section headers associated with section numbers
     private let sections = ["Name": 0, "From": 1, "To": 2, "Alarm": 3]
     
-    // Heights of fields\
+    // Keeps track of index paths
+    private let indexPaths = ["Name": NSIndexPath(forRow: 0, inSection: 0),
+        "From": NSIndexPath(forRow: 0, inSection: 1), "To": NSIndexPath(forRow: 0, inSection: 2),
+        "AlarmToggle": NSIndexPath(forRow: 0, inSection: 3),
+        "AlarmDateToggle": NSIndexPath(forRow: 1, inSection: 3),
+        "AlarmTimeDisplay": NSIndexPath(forRow: 2, inSection: 3),
+        "AlarmTimePicker": NSIndexPath(forRow: 3, inSection: 3)]
+    
+    // Heights of fields
     private let DEFAULT_CELL_HEIGHT = UITableViewCell().frame.height
+    private let PICKER_CELL_HEIGHT = UIPickerView().frame.height
+    
     private var eventNameCellHeight: CGFloat
+    
     private var eventDateStartCellHeight: CGFloat
     private var eventDateEndCellHeight: CGFloat
     
+    private var alarmToggleCellHeight: CGFloat
+    private var alarmDateToggleCellHeight: CGFloat
+    private var alarmTimeDisplayCellHeight: CGFloat
+    private var alarmTimePickerCellHeight: CGFloat
+    
+    private var selectedIndexPath: NSIndexPath?
     
     // Initialization, set default heights
     required init(coder: NSCoder) {
         eventNameCellHeight = DEFAULT_CELL_HEIGHT
+        
         eventDateStartCellHeight = DEFAULT_CELL_HEIGHT
         eventDateEndCellHeight = DEFAULT_CELL_HEIGHT
+        
+        alarmToggleCellHeight = DEFAULT_CELL_HEIGHT
+        alarmDateToggleCellHeight = 0
+        alarmTimeDisplayCellHeight = 0
+        alarmTimePickerCellHeight = 0
         
         super.init(coder: coder)
     }
@@ -66,9 +94,11 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        println(tableView.frame.size.height)
+        
         // Set tableview delegate and data source
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Disable text field user interaction, needed to allow proper table view row selection
         eventNameTextField.userInteractionEnabled = false
@@ -93,9 +123,12 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
         eventDateFormatter.dateFormat = "h:mm a"
         eventDateStartDetailsLabel.text = eventDateFormatter.stringFromDate(date!)
         eventDateEndDetailsLabel.text = eventDateFormatter.stringFromDate(nextHourDate)
+        
+        alarmSwitch.on = false
+        alarmDateSwitch.on = false
     }
-
-
+    
+    
     // When date start picker changes, update date start labels
     func updateEventDateStartLabels() {
         eventDateFormatter.dateFormat = "MMM dd, yyyy"
@@ -129,11 +162,12 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
         // Take action based on what section was chosen
         switch indexPath.section {
         case sections["Name"]!:
-            tableView.reloadData()
+            selectedIndexPath = indexPaths["Name"]
             eventNameTextField.userInteractionEnabled = true
             eventNameTextField.becomeFirstResponder()
             tableView.selectRowAtIndexPath(indexPath, animated: true, scrollPosition: .None)
         case sections["From"]!:
+            selectedIndexPath = indexPaths["From"]
             // Hide date start labels
             eventDateStartMainLabel.hidden = true
             eventDateStartDetailsLabel.hidden = true
@@ -149,6 +183,8 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
             
             tableView.endUpdates()
         case sections["To"]!:
+            selectedIndexPath = indexPaths["To"]
+            
             // Hide date end labels
             eventDateEndMainLabel.hidden = true
             eventDateEndDetailsLabel.hidden = true
@@ -163,11 +199,83 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
             cell.contentView.didAddSubview(eventDateEndPicker)
             
             tableView.endUpdates()
-        case sections["Alarm"]!:
-            println("Alarm")
         default:
             break
         }
+    }
+    
+    @IBAction func toggleAlarmOptions(sender: AnyObject) {
+        if selectedIndexPath != nil {
+            tableView.deselectRowAtIndexPath(selectedIndexPath!, animated: false)
+            selectedIndexPath = nil
+        }
+        if let alarmToggle = sender as? UISwitch {
+            if alarmToggle.on {
+                showMoreAlarmOptions()
+            }
+            else {
+                showLessAlarmOptions()
+            }
+        }
+    }
+    
+    
+    func showMoreAlarmOptions() {
+        println("***MORE***")
+        tableView.beginUpdates()
+        let alarmDateToggleCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmDateToggle"]!)
+        let alarmTimeDisplayCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmTimeDisplay"]!)
+        let alarmTimePickerCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmTimePicker"]!)
+        
+        alarmDateToggleCellHeight = PICKER_CELL_HEIGHT
+        alarmTimeDisplayCellHeight = PICKER_CELL_HEIGHT
+        alarmTimePickerCellHeight = PICKER_CELL_HEIGHT
+        
+        alarmDateToggleCell!.contentView.hidden = false
+        alarmTimeDisplayCell!.contentView.hidden = false
+        alarmTimePickerCell!.contentView.hidden = false
+        
+        println(tableView.contentSize)
+        //tableView.reloadData()
+        //resizeTableViewFrameHeight()
+        tableView.endUpdates()
+        //tableView.reloadData()
+        //resizeTableViewFrameHeight()
+        tableView.reloadData()
+        println(tableView.contentSize)
+        NSLog("(%d, %d): (%d, %d)", tableView.bounds.origin.x, tableView.bounds.origin.y,
+        tableView.bounds.width, tableView.bounds.height)
+    }
+    
+    
+    func showLessAlarmOptions() {
+        println("***LESS***")
+        tableView.beginUpdates()
+        let alarmDateToggleCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmDateToggle"]!)
+        let alarmTimeDisplayCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmTimeDisplay"]!)
+        let alarmTimePickerCell = tableView.cellForRowAtIndexPath(indexPaths["AlarmTimePicker"]!)
+        
+        alarmDateToggleCellHeight = 0
+        alarmTimeDisplayCellHeight = 0
+        alarmTimePickerCellHeight = 0
+        
+        alarmDateToggleCell!.contentView.hidden = true
+        alarmTimeDisplayCell!.contentView.hidden = true
+        //println(alarmDateToggleCell)
+        //println(alarmTimeDisplayCell)
+        ///println(alarmTimePickerCell)
+        alarmTimePickerCell!.contentView.hidden = true
+        tableView.endUpdates()
+        //tableView.reloadData()
+        //println(tableView.contentSize)
+    }
+    
+    
+    func resizeTableViewFrameHeight() {
+        var frame = tableView.frame
+        let size = self.tableView.sizeThatFits(CGSizeMake(frame.size.width, CGFloat(UINT32_MAX)))
+        frame.size.height = size.height
+        tableView.frame = frame
     }
     
     
@@ -213,17 +321,34 @@ class ChangeEventViewController: UITableViewController, UITableViewDataSource, U
     // Calculates height for rows
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         switch indexPath.section {
-            // Event name field has default height
-            case sections["Name"]!:
-                return eventNameCellHeight
-            // Event date start field changes height based on if it is selected or not
-            case sections["From"]!:
-                return eventDateStartCellHeight
-            // Event date end field changes height based on if it is selected or not
-            case sections["To"]!:
-                return eventDateEndCellHeight
+        // Event name field has default height
+        case sections["Name"]!:
+            return eventNameCellHeight
+        // Event date start field changes height based on if it is selected or not
+        case sections["From"]!:
+            return eventDateStartCellHeight
+        // Event date end field changes height based on if it is selected or not
+        case sections["To"]!:
+            return eventDateEndCellHeight
+        case sections["Alarm"]!:
+            switch indexPath.row {
+            // Alarm toggle height
+            case indexPaths["AlarmToggle"]!.row:
+                return alarmToggleCellHeight
+            // Use date toggle height
+            case indexPaths["AlarmDateToggle"]!.row:
+                return alarmDateToggleCellHeight
+            // Alarm time display height
+            case indexPaths["AlarmTimeDisplay"]!.row:
+                return alarmTimeDisplayCellHeight
+            // Alarm time picker height
+            case indexPaths["AlarmTimePicker"]!.row:
+                return alarmTimePickerCellHeight
             default:
                 return DEFAULT_CELL_HEIGHT
+            }
+        default:
+            return DEFAULT_CELL_HEIGHT
         }
     }
 }
